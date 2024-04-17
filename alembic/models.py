242a7798cc7fd621d52fcec0ba3f38129b8335e1
hfmartinez <heberthfabianmartinez@gmail.com>
2024-04-17 16:42:11 -0400

    [add] database migration in docker compose

---
+++ alembic/models.py
@@ -0,0 +1,28 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, Float
+from sqlalchemy.orm import relationship
+
+from database import Base
+
+
+class Image(Base):
+    __tablename__ = "images"
+
+    id = Column(Integer, primary_key=True)
+    image_base_64 = Column(String)
+
+    boxes = relationship("Box", back_populates="owner")
+
+
+class Box(Base):
+    __tablename__ = "boxes"
+
+    id = Column(Integer, primary_key=True)
+    x = Column(Integer)
+    y = Column(Integer)
+    w = Column(Integer)
+    h = Column(Integer)
+    confidence = Column(Float)
+    label = Column(String)
+    owner_id = Column(Integer, ForeignKey("images.id"))
+
+    owner = relationship("Image", back_populates="boxes")
